53457d8a0742b4f2ddb157ddd413ef333a96f4b7
MARxLENin <gravefall@mail.ru>
2021-03-10 21:47:56 +0500

    fix: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) {
+    return false
+  }
+  var temp = bracketsConfig.map(pair => pair.join(""))
+  
+  while (temp.some(item => str.includes(item))) {
+    for (let subStr of temp) {
+      str = str.replace(subStr, "")
+    }
+  }
+  return (str.length === 0)
 }
